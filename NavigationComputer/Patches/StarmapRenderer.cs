a85a0178cb9487916fff4d85ff326787d823d527
Michael P. Starkweather <mpstark@gmail.com>
2019-01-27 05:35:01 -0600

    Skip clearing the selected route when shift is held -- much less frustrating

--- NavigationComputer/Patches/StarmapRenderer.cs
+++ NavigationComputer/Patches/StarmapRenderer.cs
@@ -10,11 +10,19 @@ namespace NavigationComputer.Patches
     [HarmonyPatch(typeof(StarmapRenderer), "SetSelectedSystemRenderer")]
     public static class StarmapRenderer_SetSelectedSystemRenderer_Patch
     {
-        public static void Prefix(StarmapSystemRenderer systemRenderer, StarmapSystemRenderer ___currSystem)
+        public static bool Prefix(StarmapSystemRenderer systemRenderer, StarmapSystemRenderer ___currSystem)
         {
+            if (systemRenderer == null && (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)))
+            {
+                Main.HBSLog.Log("Skipping SetSelectedSystemRenderer with systemRenderer null while shift held");
+                return false;
+            }
+
             if (systemRenderer != null && systemRenderer != ___currSystem
                 && (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)))
                 ShiftClickMove.NextSelectIsShiftClick = true;
+
+            return true;
         }
     }
 }
